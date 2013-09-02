d02512b41a432e912e7f3f96bae2265790624ab7
Dean Camera <dean@fourwalledcubicle.com>
2013-09-02 19:28:22 +0200

    Refactor to make the code easier to read.

--- make-as-compatible.py
+++ make-as-compatible.py
@@ -24,15 +24,33 @@ import xml.etree.ElementTree as ET
 vsix_namespace_map = {'vsix' : 'http://schemas.microsoft.com/developer/vsx-schema/2010'}
 ET.register_namespace('', 'http://schemas.microsoft.com/developer/vsx-schema/2010')
 
-def add_supported_product(tree, type, version, name):
+
+def decompress_vsix(vsix_file, extract_path):
+    with ZipFile(vsix_file, 'r') as vsix_zip:
+        vsix_zip.extractall(extract_path)
+
+
+def compress_vsix(vsix_file, source_path):
+    with ZipFile(vsix_file, 'w') as vsix_zip:
+        for base, dirs, files in os.walk(source_path):
+           for file in files:
+              source_file = os.path.join(base, file)
+              vsix_zip.write(source_file, os.path.relpath(source_file, source_path))
+
+
+def remove_references(tree):
+    # Remove all references from the manifest
     references = tree.find(".//vsix:References", namespaces=vsix_namespace_map)
     references.clear()
 
-    supported_products = tree.find(".//vsix:SupportedProducts", namespaces=vsix_namespace_map)
 
-    new_supported_product = ET.SubElement(supported_products, "%s" % type)
+def add_supported_product(tree, type, version, name):
+    # Create a new SupportedProducts entry of the desired type, version and name
+    supported_products = tree.find(".//vsix:SupportedProducts", namespaces=vsix_namespace_map)
+    new_supported_product = ET.SubElement(supported_products, type)
     new_supported_product.set('Version', str(version))
     new_supported_product.text = name
+
 
 def make_as_compatible(input_vsix_file, desired_version):
     # Input VSIX file without extension
@@ -40,25 +58,25 @@ def make_as_compatible(input_vsix_file, desired_version):
     # Temporary folder where the extracted extension contents will be stored
     temp_folder_name = "%s_temp" % input_vsix_name
     # Input extension manifest file path
-    extension_manifest = "%s/extension.vsixmanifest" % temp_folder_name
+    extension_manifest = os.path.join(temp_folder_name, "extension.vsixmanifest")
     # Output VSIX file name
     output_vsix_file = "%s_AtmelStudio%s.vsix" % (input_vsix_name, desired_version)
 
     # Extract input extension to a temporary directory
-    with ZipFile(vsix_file, 'r') as vsix_zip:
-        vsix_zip.extractall(temp_folder_name)
+    decompress_vsix(input_vsix_file, temp_folder_name)
 
-    # Add the Atmel Studio supported product to the manifest
+    # Parse the VSIX manifest XML so that it can be modified
     vsix_manifest_tree = ET.parse(extension_manifest)
+
+    # Add the Atmel Studio supported product to the manifest, remove references
+    remove_references(vsix_manifest_tree)
     add_supported_product(vsix_manifest_tree, 'IsolatedShell', desired_version, "AtmelStudio")
+
+    # Update the manifest file with the modified contents
     vsix_manifest_tree.write(extension_manifest)
 
     # Write the updated extension to a new VSIX file
-    with ZipFile(output_vsix_file, 'w') as vsix_zip:
-        for base, dirs, files in os.walk(temp_folder_name):
-           for file in files:
-              fn = os.path.join(base, file)
-              vsix_zip.write(fn, os.path.relpath(fn, temp_folder_name))
+    compress_vsix(output_vsix_file, temp_folder_name)
 
 
 if __name__ == '__main__':
@@ -70,7 +88,7 @@ if __name__ == '__main__':
     vsix_file = sys.argv[1]
     desired_version = sys.argv[2]
 
-    if not os.path.isfile(sys.argv[1]):
+    if not os.path.isfile(vsix_file):
         print("Input extension file is not valid.")
         sys.exit(1)
 
